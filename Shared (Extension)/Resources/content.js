bb28a702dccc7277358687748aafb7cde7697d5d
Ulrik Lyngs <ulrik.lyngs@gmail.com>
2021-12-04 18:17:35 +0100

    first commit with working demo

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -1,7 +1,23 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
-});
+// content.js
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if(request.method == "changePage"){
+            feed = document.querySelector('.pedkr2u6.tn0ko95a.pnx7fd3z div[role="feed"]');
+            
+            console.log(feed.style.visibility);
+            
+            if (feed.style.visibility === "hidden") {
+                feed.style.visibility = "visible";
+                sendResponse({text: "newsfeed visible", method: "changePage"}); //same as innerText
+            } else if (feed.style.visibility === "visible") {
+                feed.style.visibility = "hidden";
+                sendResponse({text: "newsfeed hidden", method: "changePage"}); //same as innerText
+            } else {
+                feed.style.visibility = "visible";
+                sendResponse({text: "newsfeed visible", method: "changePage"}); //same as innerText
+            }
+        }
+    }
+);
 
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
-});
+
--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -1,1 +1,17 @@
-console.log("Hello World!", browser);
+// popup.js
+document.addEventListener('DOMContentLoaded', function() {
+  var checkButton = document.getElementById('feedToggle');
+  checkButton.addEventListener('click', function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {method: "changePage"}, function(response) {
+          if(response.method == "changePage"){
+              if(response.text === "newsfeed visible"){
+                  checkButton.innerHTML = "Hide newsfeed"
+              } else {
+                  checkButton.innerHTML = "Show newsfeed";
+              }
+          }
+        });
+      });
+  }, false);
+}, false);
