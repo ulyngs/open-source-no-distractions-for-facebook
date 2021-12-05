4172e9e4a9ca240c1937b1a25ca4b0d273586d2e
Ulrik Lyngs <ulrik.lyngs@gmail.com>
2021-12-05 10:01:57 +0100

    clean up code

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -4,8 +4,6 @@ chrome.runtime.onMessage.addListener(
         if(request.method == "changeFeed"){
             feed = document.querySelector('.pedkr2u6.tn0ko95a.pnx7fd3z div[role="feed"]');
             
-            console.log(feed.style.visibility);
-            
             if (feed.style.visibility === "hidden") {
                 feed.style.visibility = "visible";
                 sendResponse({text: "newsfeed visible", method: "changeFeed"});
