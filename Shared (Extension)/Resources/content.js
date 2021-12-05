d338c837a91e0f770c73ef61aeb603fd93dd5b27
Ulrik Lyngs <ulrik.lyngs@gmail.com>
2021-12-05 08:45:43 +0100

    rename variable sensibly

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -1,20 +1,20 @@
 // content.js
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        if(request.method == "changePage"){
+        if(request.method == "changeFeed"){
             feed = document.querySelector('.pedkr2u6.tn0ko95a.pnx7fd3z div[role="feed"]');
             
             console.log(feed.style.visibility);
             
             if (feed.style.visibility === "hidden") {
                 feed.style.visibility = "visible";
-                sendResponse({text: "newsfeed visible", method: "changePage"}); //same as innerText
+                sendResponse({text: "newsfeed visible", method: "changeFeed"});
             } else if (feed.style.visibility === "visible") {
                 feed.style.visibility = "hidden";
-                sendResponse({text: "newsfeed hidden", method: "changePage"}); //same as innerText
+                sendResponse({text: "newsfeed hidden", method: "changeFeed"});
             } else {
                 feed.style.visibility = "visible";
-                sendResponse({text: "newsfeed visible", method: "changePage"}); //same as innerText
+                sendResponse({text: "newsfeed visible", method: "changeFeed"});
             }
         }
     }
--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -3,8 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
   var checkButton = document.getElementById('feedToggle');
   checkButton.addEventListener('click', function() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {method: "changePage"}, function(response) {
-          if(response.method == "changePage"){
+        chrome.tabs.sendMessage(tabs[0].id, {method: "changeFeed"}, function(response) {
+          if(response.method == "changeFeed"){
               if(response.text === "newsfeed visible"){
                   checkButton.innerHTML = "Hide newsfeed"
               } else {
