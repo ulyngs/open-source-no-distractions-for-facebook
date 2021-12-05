a7d539bb45d2975cdd0180d571b259c0fa7b9c0f
Ulrik Lyngs <ulrik.lyngs@gmail.com>
2021-12-05 09:13:28 +0100

    add toggle for chat

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -17,6 +17,28 @@ chrome.runtime.onMessage.addListener(
                 sendResponse({text: "newsfeed visible", method: "changeFeed"});
             }
         }
+        
+        if(request.method == "changeChat"){
+            chatElements = document.querySelectorAll('div[data-pagelet="RightRail"] div[data-visualcompletion="ignore-dynamic"] > div[class = "l9j0dhe7"');
+            
+            if (chatElements[0].style.visibility === "hidden") {
+                for (var i = 0, max = chatElements.length; i < max; i++) {
+                    chatElements[i].style.visibility = "visible";
+                }
+                
+                sendResponse({text: "chat visible", method: "changeChat"});
+            } else if (chatElements[0].style.visibility === "visible") {
+                for (var i = 0, max = chatElements.length; i < max; i++) {
+                    chatElements[i].style.visibility = "hidden";
+                }
+                sendResponse({text: "chat hidden", method: "changeChat"});
+            } else {
+                for (var i = 0, max = chatElements.length; i < max; i++) {
+                    chatElements[i].style.visibility = "visible";
+                }
+                sendResponse({text: "chat visible", method: "changeChat"});
+            }
+        }
     }
 );
 
--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -1,17 +1,32 @@
 // popup.js
 document.addEventListener('DOMContentLoaded', function() {
-  var checkButton = document.getElementById('feedToggle');
-  checkButton.addEventListener('click', function() {
+    var checkButton = document.getElementById('feedToggle');
+    checkButton.addEventListener('click', function() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {method: "changeFeed"}, function(response) {
-          if(response.method == "changeFeed"){
-              if(response.text === "newsfeed visible"){
-                  checkButton.innerHTML = "Hide newsfeed"
-              } else {
-                  checkButton.innerHTML = "Show newsfeed";
-              }
-          }
+            if(response.method == "changeFeed"){
+                if(response.text === "newsfeed visible"){
+                    checkButton.innerHTML = "Hide newsfeed"
+                } else {
+                    checkButton.innerHTML = "Show newsfeed";
+                }
+            }
         });
       });
-  }, false);
+    }, false);
+    
+    var chatButton = document.getElementById('chatToggle');
+    chatButton.addEventListener('click', function() {
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+          chrome.tabs.sendMessage(tabs[0].id, {method: "changeChat"}, function(response) {
+              if(response.method == "changeChat"){
+                  if(response.text === "chat visible"){
+                      chatButton.innerHTML = "Hide chat"
+                  } else {
+                      chatButton.innerHTML = "Show chat";
+                  }
+              }
+          });
+        });
+    }, false);
 }, false);
