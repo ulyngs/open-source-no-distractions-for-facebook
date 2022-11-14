227d82b0669d0b73f9b5725af5955fa2b7a95f21
Ulrik Lyngs <ulrik.lyngs@gmail.com>
2022-11-14 09:14:15 +0000

    put the toggling in a function to simplify code

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -1,12 +1,4 @@
 // content.js
-
-
-function toggleElement(method){
-    
-}
-
-
-
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         feed = document.querySelector('div[role="main"] h3.x1heor9g.x1qlqyl8.x1pd3egz.x1a2a7pz.xzpqnlu.x1hyvwdk.xjm9jq1.x6ikm8r.x10wlt62.x10l6tqk.x1i1rx1s + div');
@@ -68,110 +60,92 @@ chrome.runtime.onMessage.addListener(
         }
         
         // change visibility
-        if(request.method == "changeFeed"){
-            if (feed.style.visibility === "hidden") {
-                feed.style.visibility = "visible";
-                sendResponse({text: "newsfeed visible", method: "changeFeed"});
-            } else if (feed.style.visibility === "visible") {
-                feed.style.visibility = "hidden";
-                sendResponse({text: "newsfeed hidden", method: "changeFeed"});
+        function toggleElement(element, css_to_change, is_multi_element){
+            if (is_multi_element){
+                element_to_check = element[0];
             } else {
-                feed.style.visibility = "visible";
-                sendResponse({text: "newsfeed visible", method: "changeFeed"});
+                element_to_check = element;
             }
+            
+            if (css_to_change === "visibility"){
+                if (element_to_check.style.visibility === "hidden") {
+                    if (is_multi_element){
+                        for (var i = 0, max = element.length; i < max; i++) {
+                            element[i].style.visibility = "visible";
+                        }
+                    } else {
+                        element.style.visibility = "visible";
+                    }
+                    
+                } else if (element_to_check.style.visibility === "visible") {
+                    if (is_multi_element){
+                        for (var i = 0, max = element.length; i < max; i++) {
+                            element[i].style.visibility = "hidden";
+                        }
+                    } else {
+                        element.style.visibility = "hidden";
+                    }
+                } else {
+                    if (is_multi_element){
+                        for (var i = 0, max = element.length; i < max; i++) {
+                            element[i].style.visibility = "visible";
+                        }
+                    } else {
+                        element.style.visibility = "visible";
+                    }
+                }
+            } else if (css_to_change === "display"){
+                if (element_to_check.style.display === "none") {
+                    if (is_multi_element){
+                        for (var i = 0, max = element.length; i < max; i++) {
+                            element[i].style.display = "block";
+                        }
+                    } else {
+                        element.style.display = "block";
+                    }
+                    
+                } else if (element_to_check.style.display === "block") {
+                    if (is_multi_element){
+                        for (var i = 0, max = element.length; i < max; i++) {
+                            element[i].style.display = "none";
+                        }
+                    } else {
+                        element.style.display = "none";
+                    }
+                } else {
+                    if (is_multi_element){
+                        for (var i = 0, max = element.length; i < max; i++) {
+                            element[i].style.display = "block";
+                        }
+                    } else {
+                        element.style.display = "block";
+                    }
+                }
+            }
+        };
+        
+        if(request.method == "changeFeed"){
+            toggleElement(feed, "visibility", is_multi_element = false);
         }
         
         if(request.method == "changeMobileFeed"){
-            if (mobileFeed[1].style.display === "none") {
-                //mobileFeed.style.display = "block";
-                for (var i = 0, max = mobileFeed.length; i < max; i++) {
-                    mobileFeed[i].style.display = "block";
-                }
-                
-                sendResponse({text: "mobileFeed visible", method: "changeMobileFeed"});
-            } else if (mobileFeed[1].style.display === "block") {
-                //mobileFeed.style.display = "none";
-                for (var i = 0, max = mobileFeed.length; i < max; i++) {
-                    mobileFeed[i].style.display = "none";
-                }
-                
-                sendResponse({text: "mobileFeed hidden", method: "changeMobileFeed"});
-            } else {
-                //mobileFeed.style.display = "block";
-                for (var i = 0, max = mobileFeed.length; i < max; i++) {
-                    mobileFeed[i].style.display = "block";
-                }
-                
-                sendResponse({text: "mobileFeed visible", method: "changeMobileFeed"});
-            }
+            toggleElement(mobileFeed, "display", is_multi_element = true);
         }
         
         if(request.method == "changeChat"){
-            if (chatElements[0].style.visibility === "hidden") {
-                for (var i = 0, max = chatElements.length; i < max; i++) {
-                    chatElements[i].style.visibility = "visible";
-                }
-                
-                sendResponse({text: "chat visible", method: "changeChat"});
-            } else if (chatElements[0].style.visibility === "visible") {
-                for (var i = 0, max = chatElements.length; i < max; i++) {
-                    chatElements[i].style.visibility = "hidden";
-                }
-                sendResponse({text: "chat hidden", method: "changeChat"});
-            } else {
-                for (var i = 0, max = chatElements.length; i < max; i++) {
-                    chatElements[i].style.visibility = "visible";
-                }
-                sendResponse({text: "chat visible", method: "changeChat"});
-            }
+            toggleElement(chatElements, "visibility", is_multi_element = true);
         }
         
         if(request.method == "changeStories"){
-            if (stories.style.display === "none") {
-                stories.style.display = "block";
-                sendResponse({text: "stories visible", method: "changeStories"});
-            } else if (stories.style.display === "block") {
-                stories.style.display = "none";
-                sendResponse({text: "stories hidden", method: "changeStories"});
-            } else {
-                stories.style.display = "block";
-                sendResponse({text: "stories visible", method: "changeStories"});
-            }
+            toggleElement(stories, "display", is_multi_element = false);
         }
         
         if(request.method == "changeStoriesMobile"){
-            if (storiesMobile.style.display === "none") {
-                storiesMobile.style.display = "block";
-                sendResponse({text: "stories visible", method: "changeStoriesMobile"});
-            } else if (storiesMobile.style.display === "block") {
-                storiesMobile.style.display = "none";
-                sendResponse({text: "stories hidden", method: "changeStoriesMobile"});
-            } else {
-                storiesMobile.style.display = "block";
-                sendResponse({text: "stories visible", method: "changeStoriesMobile"});
-            }
+            toggleElement(storiesMobile, "display", is_multi_element = false);
         }
         
         if(request.method == "changeWatchMobile"){
-            if (watchMobile[1].style.visibility === "hidden") {
-                //watchMobile.style.display = "block";
-                for (var i = 0, max = watchMobile.length; i < max; i++) {
-                    watchMobile[i].style.visibility = "visible";
-                }
-                sendResponse({text: "watch visible", method: "changeWatchMobile"});
-            } else if (watchMobile[1].style.visibility === "visible") {
-                //watchMobile.style.display = "none";
-                for (var i = 0, max = watchMobile.length; i < max; i++) {
-                    watchMobile[i].style.visibility = "hidden";
-                }
-                sendResponse({text: "watch hidden", method: "changeWatchMobile"});
-            } else {
-                //watchMobile[1].style.display = "block";
-                for (var i = 0, max = watchMobile.length; i < max; i++) {
-                    watchMobile[i].style.visibility = "visible";
-                }
-                sendResponse({text: "watch visible", method: "changeWatchMobile"});
-            }
+            toggleElement(watchMobile, "visibility", is_multi_element = true);
         }
     }
 );
