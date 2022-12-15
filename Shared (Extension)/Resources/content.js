187842e92dfb324d458ae44e6cd953ad06a75897
Ulrik Lyngs <ulrik.lyngs@gmail.com>
2022-12-15 19:26:42 +0100

    move extension to syntax used in mindshield

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -1,153 +1,92 @@
-// content.js
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        feed = document.querySelector('#ssrb_feed_start + div');
-        mobileFeed = document.querySelectorAll('#screen-root div[data-screen-id="65549"] > div[data-mcomponent="MContainer"] > div.m.displayed:nth-child(n+6)');
-        chatElements = document.querySelectorAll('div[role="complementary"] div[data-visualcompletion="ignore-dynamic"] > div.x1n2onr6');
-        stories = document.querySelector('.x78zum5.x1q0g3np.xl56j7k.x1yztbdb.x1y1aw1k');
-        storiesMobile = document.querySelector('#screen-root div[data-screen-id="65549"] > div[data-mcomponent="MContainer"] > div.m.displayed:nth-of-type(4)');
-        watchMobile = document.querySelectorAll('#screen-root div[data-mcomponent="MContainer"] > div.m.displayed:nth-child(2) > div.m:nth-child(2) > div.m:nth-child(4) > div.m:nth-child(1), #screen-root div[data-mcomponent="MContainer"] > div.m.displayed:nth-child(2) > div.m:nth-child(2) > div.m:nth-child(4) > div.m:nth-child(3)');
-        
-        function checkElement(method_to_send, element, css_to_change, is_multi_element){
-            if (is_multi_element){
-                element_to_check = element[0];
-            } else {
-                element_to_check = element;
-            }
-            
-            if (css_to_change === "visibility"){
-                if (element_to_check.style.visibility === "hidden") {
-                    sendResponse({text: "hidden", method: method_to_send});
-                } else if (element_to_check.style.visibility === "visible") {
-                    sendResponse({text: "visible", method: method_to_send});
-                } else {
-                    sendResponse({text: "hidden", method: method_to_send});
-                }
-            } else if (css_to_change === "display"){
-                if (element_to_check.style.display === "none") {
-                    sendResponse({text: "hidden", method: method_to_send});
-                } else if (element_to_check.style.display === "block") {
-                    sendResponse({text: "visible", method: method_to_send});
-                } else {
-                    sendResponse({text: "hidden", method: method_to_send});
-                }
-            }
-        };
-        
-        // check for visibility
-        if(request.method == "checkFeed"){
-            checkElement("checkFeed", feed, "visibility", is_multi_element = false);
-        }
-        
-        if(request.method == "checkMobileFeed"){
-            checkElement("checkMobileFeed", feed, "display", is_multi_element = true);
-        }
-        
-        if(request.method == "checkChat"){
-            checkElement("checkChat", chatElements, "visibility", is_multi_element = true);
-        }
-        
-        if(request.method == "checkStories"){
-            checkElement("checkStories", stories, "display", is_multi_element = false);
-        }
-        
-        if(request.method == "checkStoriesMobile"){
-            checkElement("checkStoriesMobile", storiesMobile, "display", is_multi_element = false);
-        }
-        
-        if(request.method == "checkWatchMobile"){
-            checkElement("checkStoriesMobile", storiesMobile, "visibility", is_multi_element = true);
-        }
-        
-        // change visibility
-        function toggleElement(element, css_to_change, is_multi_element){
-            
-            if (is_multi_element){
-                element_to_check = element[0];
-            } else {
-                element_to_check = element;
-            }
-            
-            if (css_to_change === "visibility"){
-                if (element_to_check.style.visibility === "hidden") {
-                    if (is_multi_element){
-                        for (var i = 0, max = element.length; i < max; i++) {
-                            element[i].style.visibility = "visible";
-                        }
-                    } else {
-                        element.style.visibility = "visible";
-                    }
-                    
-                } else if (element_to_check.style.visibility === "visible") {
-                    if (is_multi_element){
-                        for (var i = 0, max = element.length; i < max; i++) {
-                            element[i].style.visibility = "hidden";
-                        }
-                    } else {
-                        element.style.visibility = "hidden";
-                    }
-                } else {
-                    if (is_multi_element){
-                        for (var i = 0, max = element.length; i < max; i++) {
-                            element[i].style.visibility = "visible";
-                        }
-                    } else {
-                        element.style.visibility = "visible";
-                    }
-                }
-            } else if (css_to_change === "display"){
-                if (element_to_check.style.display === "none") {
-                    if (is_multi_element){
-                        for (var i = 0, max = element.length; i < max; i++) {
-                            element[i].style.display = "block";
-                        }
-                    } else {
-                        element.style.display = "block";
-                    }
-                    
-                } else if (element_to_check.style.display === "block") {
-                    if (is_multi_element){
-                        for (var i = 0, max = element.length; i < max; i++) {
-                            element[i].style.display = "none";
-                        }
-                    } else {
-                        element.style.display = "none";
-                    }
-                } else {
-                    if (is_multi_element){
-                        for (var i = 0, max = element.length; i < max; i++) {
-                            element[i].style.display = "block";
-                        }
-                    } else {
-                        element.style.display = "block";
-                    }
-                }
-            }
-        };
-        
-        if(request.method == "changeFeed"){
-            toggleElement(feed, "visibility", is_multi_element = false);
-        }
-        
-        if(request.method == "changeMobileFeed"){
-            toggleElement(mobileFeed, "display", is_multi_element = true);
-        }
-        
-        if(request.method == "changeChat"){
-            toggleElement(chatElements, "visibility", is_multi_element = true);
-        }
-        
-        if(request.method == "changeStories"){
-            toggleElement(stories, "display", is_multi_element = false);
-        }
-        
-        if(request.method == "changeStoriesMobile"){
-            toggleElement(storiesMobile, "display", is_multi_element = false);
-        }
-        
-        if(request.method == "changeWatchMobile"){
-            toggleElement(watchMobile, "visibility", is_multi_element = true);
-        }
-    }
-);
-
+ (function() {
+     /**
+      * Check and set a global guard variable.
+      * If this content script is injected into the same page again,
+      * it will do nothing next time.
+      */
+     if (window.hasRun) {
+       return;
+     }
+     window.hasRun = true;
+     
+     const elementsThatCanBeHidden = [ "facebookFeed", "facebookWatch", "facebookNotifications", "facebookStories", "facebookChat" ];
+     
+     // Facebook CSS
+     const facebookFeedCssOn = '#ssrb_feed_start + div { visibility: visible; } #m_news_feed_stream, #screen-root div[data-screen-id="65549"] > div[data-mcomponent="MContainer"] > div.m.displayed:nth-child(n+6) { display: block; }'
+     const facebookFeedCssOff = '#ssrb_feed_start + div { visibility: hidden; } #m_news_feed_stream, #screen-root div[data-screen-id="65549"] > div[data-mcomponent="MContainer"] > div.m.displayed:nth-child(n+6) { display: none; }'
+     
+     const facebookWatchCssOn = '#screen-root div[data-mcomponent="MScreen"] div[data-mcomponent="MContainer"] div[data-mcomponent="MContainer"]:nth-child(2) div[role="button"]:nth-child(4) {display: flex;}'
+     const facebookWatchCssOff = '#screen-root div[data-mcomponent="MScreen"] div[data-mcomponent="MContainer"] div[data-mcomponent="MContainer"]:nth-child(2) div[role="button"]:nth-child(4) {display: none;}'
+     
+     const facebookNotificationsCssOn = 'div[aria-label^="Notifications"], #screen-root div[data-mcomponent="MScreen"] div[data-mcomponent="MContainer"] div[data-mcomponent="MContainer"]:nth-child(2) div[role="button"]:nth-child(5) div[data-mcomponent="MContainer"]:nth-child(3) {display: flex;}'
+     const facebookNotificationsCssOff = 'div[aria-label^="Notifications"], #screen-root div[data-mcomponent="MScreen"] div[data-mcomponent="MContainer"] div[data-mcomponent="MContainer"]:nth-child(2) div[role="button"]:nth-child(5) div[data-mcomponent="MContainer"]:nth-child(3) {display: none;}'
+     
+     const facebookChatCssOn = 'div[role="complementary"] div[data-visualcompletion="ignore-dynamic"] > div.x1n2onr6 { visibility: visible; }'
+     const facebookChatCssOff = 'div[role="complementary"] div[data-visualcompletion="ignore-dynamic"] > div.x1n2onr6 { visibility: hidden; }'
+     
+     const facebookStoriesCssOn = '.x78zum5.x1q0g3np.xl56j7k.x1yztbdb.x1y1aw1k { display: block; } #screen-root div[data-mcomponent="MContainer"] > div[data-mcomponent="MContainer"]:has(div[aria-label*="story"]) { display: flex;}'
+     const facebookStoriesCssOff = '.x78zum5.x1q0g3np.xl56j7k.x1yztbdb.x1y1aw1k { display: none; } #screen-root div[data-mcomponent="MContainer"] > div[data-mcomponent="MContainer"]:has(div[aria-label*="story"]) { display: none;}'
+     
+     
+     // function to create style element with the specified CSS content
+     function createStyleElement(some_style_id, some_css){
+         if(!document.getElementById(some_style_id)){
+             var styleElement = document.createElement("style");
+             styleElement.id = some_style_id;
+             document.head.appendChild(styleElement).innerHTML = some_css;
+         } else {
+             document.getElementById(some_style_id).innerHTML = some_css;
+         };
+     };
+     
+     elementsThatCanBeHidden.forEach(function (item) {
+         console.log("create the styles " + item);
+         var styleName = item + "Style";
+         var key = item + "Status";
+         
+         browser.storage.sync.get(key, function(result) {
+             if (result[key] == true){
+                 createStyleElement(styleName, eval(item + "CssOn"));
+             } else {
+                 createStyleElement(styleName, eval(item + "CssOff"));
+             };
+         });
+     });
+     
+     // let the popup ask for the current status of the elements and of the saved state
+     chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+         function checkStyleStatus(currentStyle, some_css_for_shown){
+             if (currentStyle == undefined){
+                 sendResponse({text: "not on active tab"});
+             } else if (currentStyle.innerHTML === some_css_for_shown) {
+                 sendResponse({text: "visible"});
+             } else {
+                 sendResponse({text: "hidden"});
+             };
+         };
+     
+         if(request.method === "check"){
+             var currentStyle = document.getElementById(request.element + "Style");
+     
+             checkStyleStatus(currentStyle, eval(request.element + 'CssOn'));
+         };
+     });
+     
+     // let the content script toggle elements when the popup asks for it
+     function toggleHiding(some_style_id, css_shown, css_hidden, status){
+         var styleElement = document.getElementById(some_style_id);
+     
+         if(status == true){
+             styleElement.innerHTML = css_shown;
+         } else {
+             styleElement.innerHTML = css_hidden;
+         };
+     };
+     
+     browser.runtime.onMessage.addListener((message) => {
+         // toggle hiding when popup asks
+         if(message.method === "change"){
+             toggleHiding(message.element + 'Style', eval(message.element + 'CssOn'), eval(message.element + 'CssOff'),  message.status);
+         };
+     });
+ })();
--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -2,183 +2,70 @@
 // https://developer.chrome.com/docs/extensions/mv3/messaging/
 
 document.addEventListener('DOMContentLoaded', function() {
+    const elementsThatCanBeHidden = [ "facebookFeed", "facebookWatch", "facebookNotifications", "facebookStories", "facebookChat" ];
     
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        var activeTab = tabs[0].url;
-        // if we're on FB, then set the status of the checkboxes
-        if (activeTab.indexOf("://m.facebook.com/") > -1 || activeTab.indexOf("://www.facebook.com/") > -1){
+    // set checkboxes according to current status
+    function setCheckboxState(element_to_check, id_of_toggle){
+        var currentToggle = document.getElementById(id_of_toggle);
+        
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
             
-            chrome.tabs.sendMessage(tabs[0].id, {method: "checkFeed"}, function(response) {
-                
-                var feedCheckbox = document.getElementById('feedToggle');
-                
-                if(response.method == "checkFeed"){
-                    if(response.text === "visible"){
-                        feedCheckbox.checked = true;
-                    } else {
-                        feedCheckbox.checked = false;
-                    }
+            chrome.tabs.sendMessage(tabs[0].id, { method: "check", element: element_to_check }, function(response){
+                // if the checkbox is for a page that's different from the one we're on, set to its saved state
+                if (response.text === "not on active tab") {
+                    elementsThatCanBeHidden.forEach(function (element) {
+                        var key = element_to_check + "Status";
+                        
+                        browser.storage.sync.get(key, function(result) {
+                            currentToggle.checked = result[key];
+                        });
+                    });
+                    // otherwise set it to what's currently visible on the page
+                } else if (response.text === "visible"){
+                    currentToggle.checked = true;
+                } else {
+                    currentToggle.checked = false;
                 }
             });
+        });
+    };
+    
+    function delay(time) {
+        return new Promise(resolve => setTimeout(resolve, time));
+    }
+    
+    
+    var saveButton = document.querySelector('#saveButton');
+    
+    saveButton.addEventListener('click', (e) => {
+        // save the state of the checkboxes to local storage
+        elementsThatCanBeHidden.forEach(function (element) {
+            var key = element + "Status";
             
-            chrome.tabs.sendMessage(tabs[0].id, {method: "checkMobileFeed"}, function(response) {
-                
-                var mobileFeedCheckbox = document.getElementById('feedMobileToggle');
-                
-                if(response.method == "checkMobileFeed"){
-                    if(response.text === "visible"){
-                        mobileFeedCheckbox.checked = true;
-                    } else {
-                        mobileFeedCheckbox.checked = false;
-                    }
-                }
-            });
-            
-            chrome.tabs.sendMessage(tabs[0].id, {method: "checkChat"}, function(response) {
-                
-                var chatCheckbox = document.getElementById('chatToggle');
-                
-                if(response.method == "checkChat"){
-                    if(response.text === "visible"){
-                        chatCheckbox.checked = true;
-                    } else {
-                        chatCheckbox.checked = false;
-                    }
-                }
-            });
-            
-            chrome.tabs.sendMessage(tabs[0].id, {method: "checkStories"}, function(response) {
-                
-                var storiesCheckbox = document.getElementById('storiesToggle');
-                
-                if(response.method == "checkStories"){
-                    if(response.text === "visible"){
-                        storiesCheckbox.checked = true;
-                    } else {
-                        storiesCheckbox.checked = false;
-                    }
-                }
-            });
-            
-            chrome.tabs.sendMessage(tabs[0].id, {method: "checkStoriesMobile"}, function(response) {
-                
-                var storiesCheckboxMobile = document.getElementById('storiesToggleMobile');
-                
-                if(response.method == "checkStoriesMobile"){
-                    if(response.text === "visible"){
-                        storiesCheckboxMobile.checked = true;
-                    } else {
-                        storiesCheckboxMobile.checked = false;
-                    }
-                }
-            });
-            
-            chrome.tabs.sendMessage(tabs[0].id, {method: "checkWatchMobile"}, function(response) {
-                
-                var watchCheckboxMobile = document.getElementById('watchToggleMobile');
-                
-                if(response.method == "checkWatchMobile"){
-                    if(response.text === "visible"){
-                        watchCheckboxMobile.checked = true;
-                    } else {
-                        watchCheckboxMobile.checked = false;
-                    }
-                }
-            });
-            
-        }
+            browser.storage.sync.set({ [key]: document.getElementById(element + "Toggle").checked });
+        });
+    
+        e.target.setAttribute("value", "......");
+        delay(250).then(() => e.target.setAttribute("value", "Saved!"));
+        delay(1500).then(() => e.target.setAttribute("value", "Save settings"));
+    })
+    
+    
+    // assign functions to the checkboxes
+    function assignCheckBoxFunction(element_to_change, id_of_toggle){
+        var currentToggle = document.getElementById(id_of_toggle);
+        
+        // make it hide/show on mac
+        currentToggle.addEventListener('click', function() {
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, { method: "change", element: element_to_change, status: currentToggle.checked });
+              });
+            }, false);
+    };
+    
+    elementsThatCanBeHidden.forEach(function (item) {
+        setCheckboxState(item, item + "Toggle");
+        assignCheckBoxFunction(item, item + "Toggle");
     });
-    
-    
-    var checkButton = document.getElementById('feedToggle');
-    checkButton.addEventListener('click', function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {method: "changeFeed"}, function(response) {
-            
-            if(response.method == "changeFeed"){
-                if(response.text === "newsfeed visible"){
-                    console.log("Newsfeed is visible")
-                } else {
-                    console.log("Newsfeed is hidden")
-                }
-            }
-        });
-      });
-    }, false);
-    
-    var feedMobileButton = document.getElementById('feedMobileToggle');
-    feedMobileButton.addEventListener('click', function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {method: "changeMobileFeed"}, function(response) {
-            if(response.method == "changeMobileFeed"){
-                if(response.text === "newsfeed visible"){
-                    console.log("Newsfeed is visible")
-                } else {
-                    console.log("Newsfeed is hidden")
-                }
-            }
-        });
-      });
-    }, false);
-    
-    var chatButton = document.getElementById('chatToggle');
-    chatButton.addEventListener('click', function() {
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-          chrome.tabs.sendMessage(tabs[0].id, {method: "changeChat"}, function(response) {
-              if(response.method == "changeChat"){
-                  if(response.text === "chat visible"){
-                      console.log("Chat is visible")
-                  } else {
-                      console.log("Chat is hidden")
-                  }
-              }
-          });
-        });
-    }, false);
-    
-    var storiesButton = document.getElementById('storiesToggle');
-    storiesButton.addEventListener('click', function() {
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-          chrome.tabs.sendMessage(tabs[0].id, {method: "changeStories"}, function(response) {
-              if(response.method == "changeStories"){
-                  if(response.text === "stories visible"){
-                      console.log("Stories are visible")
-                  } else {
-                      console.log("Stories are hidden")
-                  }
-              }
-          });
-        });
-    }, false);
-    
-    var storiesButtonMobile = document.getElementById('storiesToggleMobile');
-    storiesButtonMobile.addEventListener('click', function() {
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-          chrome.tabs.sendMessage(tabs[0].id, {method: "changeStoriesMobile"}, function(response) {
-              if(response.method == "changeStoriesMobile"){
-                  if(response.text === "stories visible"){
-                      console.log("Stories are visible")
-                  } else {
-                      console.log("Stories are hidden")
-                  }
-              }
-          });
-        });
-    }, false);
-    
-    var watchButtonMobile = document.getElementById('watchToggleMobile');
-    watchButtonMobile.addEventListener('click', function() {
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-          chrome.tabs.sendMessage(tabs[0].id, {method: "changeWatchMobile"}, function(response) {
-              if(response.method == "changeWatchMobile"){
-                  if(response.text === "watch visible"){
-                      console.log("Watch prompt is visible")
-                  } else {
-                      console.log("Watch prompt is hidden")
-                  }
-              }
-          });
-        });
-    }, false);
     
 }, false);
